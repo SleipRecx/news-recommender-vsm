d97aa9badc772911a989afd15722a8765a467312
Simen Johansen <>
2018-02-20 13:33:58 +0100

    .

--- src/collaborative_filtering.py
+++ src/collaborative_filtering.py
@@ -1,9 +1,14 @@
+import os
 from surprise import KNNBasic
 from surprise import Dataset
 from surprise import Reader
 
-reader = Reader(line_format='user item rating', sep='  ')
-data = Dataset.load_from_file('data/dataset1.txt', reader=reader)
+input_fname = '../data/dataset1.txt'
+rootPath = os.path.abspath('.')
+input_file = rootPath + os.sep + input_fname
+
+reader = Reader(line_format='user item rating', sep='\t')
+data = Dataset.load_from_file('dataset1.txt', reader=reader)
 
 sim_options = {'name': 'cosine',
                'user_based': True}
