8fa67a3721b4128c871efa1bfbf775482433d95b
Markus Andresen <markus.andresen94@gmail.com>
2018-03-09 14:12:11 +0100

    more data

--- src/create_data.py
+++ src/create_data.py
@@ -20,51 +20,75 @@ def create_article_dict(filename: str, output: Dict = None) -> Dict:
             if 'id' in article:
                 key = article['id']
                 if key not in output:
-                    obj = dict(id=article['id'],
+                    obj = dict(_id=article['id'],
                                title=article['title'],
                                url=article['url'],
                                pluss='pluss' in article['url'],
                                authors=article['author'] if 'author' in article else None,
                                keywords=article['keywords'].split(',') if 'keywords' in article else None,
                                categories=article['category1'].split('|') if 'category1' in article else None,
-                               publishTime=article['publishTime'],
+                               publishTime=article['publishTime'] if 'publishTime' in article else None,
                                profiles=article['profile'][3:])
                     output[key] = obj
     return output
 
 
-def create_user_dict(filename: str) -> Dict:
-    output = defaultdict(list)
+def create_user_dict(filename: str, output: Dict = None) -> Dict:
+    if output is None:
+        output = {}
     with open(filename, 'r') as fil:
         counter = 0
         for line in fil:
             event = json.loads(line.strip())
             if 'id' in event:
-                obj = dict(userId=event['userId'],
-                           articleId=event['id'],
-                           eventId=event['eventId'],
-                           url=event['url'],
-                           deviceType=event['deviceType'],
-                           os=event['os'],
-                           city=event['city'] if 'city' in event else None,
-                           region=event['region'] if 'region' in event else None,
-                           country=event['country'] if 'country' in event else None,
-                           time=event['time'],
-                           activeTime=event['activeTime'] if 'activeTime' in event else None,
-                           sessionStart=event['sessionStart'],
-                           sessionStop=event['sessionStop'],
-                           referrerHost=event['referrerHostClass'] if 'referrerHostClass' in event else None,
-                           referrerUrl=event['referrerUrl'] if 'referrerUrl' in event else None,
-                           referrerNetwork=event['referrerSocialNetwork'] if 'referrerSocialNetwork' in event else None,
-                           referrerSearchEngine=event['referrerSearchEngine'] if 'referrerSearchEngine' in event else None)
-                output[event['userId']].append(obj)
-                if counter == 1000:
-                    break
+                user = dict(_id=event['userId'],
+                            city=event['city'] if 'city' in event else None,
+                            region=event['region'] if 'region' in event else None,
+                            country=event['country'] if 'country' in event else None,
+                            events=[]
+                            )
+
+                my_event = dict(eventId=event['eventId'],
+                                articleId=event['id'],
+                                deviceType=event['deviceType'],
+                                os=event['os'],
+                                # url=event['url'],
+                                time=event['time'],
+                                activeTime=event['activeTime'] if 'activeTime' in event else None,
+                                sessionStart=event['sessionStart'],
+                                sessionStop=event['sessionStop'],
+                                referrerHost=event['referrerHostClass'] if 'referrerHostClass' in event else None,
+                                referrerUrl=event['referrerUrl'] if 'referrerUrl' in event else None,
+                                referrerNetwork=event[
+                                    'referrerSocialNetwork'] if 'referrerSocialNetwork' in event else None,
+                                referrerSearchEngine=event[
+                                    'referrerSearchEngine'] if 'referrerSearchEngine' in event else None)
+                if user['_id'] in output:
+                    output[user['_id']]['events'].append(my_event)
+                else:
+                    user['events'].append(my_event)
+                    output[user['_id']] = user
                 counter += 1
+
     return output
 
 
+def save_clean_one_week_articles(filename: str):
+    output = {}
+    for i in range(1, 8):
+        print("Reading file:", i)
+        create_article_dict('../data/2017010' + str(i), output)
+    save_dict_as_json('articles', output)
+
+
+def save_clean_one_week_users(filename: str):
+    output = {}
+    for i in range(1, 8):
+        print("Reading file:", i)
+        create_user_dict('../data/2017010' + str(i), output)
+    save_dict_as_json('articles', output)
+
+
 if __name__ == '__main__':
-    users = create_user_dict('data/20170107')
-    with open('tjommi.json', 'w') as out_file:
-        out_file.write(json.dumps(users, indent=2, ensure_ascii=False))
+    save_clean_one_week_articles("articles")
+    save_clean_one_week_users("users")
