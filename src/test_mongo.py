b44e9ea4d0b78ed7c2c13ce118a8391869e5e878
Simen Johansen <>
2018-03-20 16:11:50 +0100

    working tfidf concept

--- src/test_mongo.py
+++ src/test_mongo.py
@@ -9,7 +9,10 @@ import math
 from bson import SON
 from collections import defaultdict
 from pymongo import MongoClient
-
+import numpy as np
+from gensim.corpora import Dictionary
+from gensim.models import TfidfModel
+from gensim.similarities import MatrixSimilarity
 
 client = MongoClient("mongodb://Gsfbretsd:5erfFSTYUfnd@167.99.45.145/adressa_ofc")
 # client = MongoClient("mongodb://gutta:gutta@ds261088.mlab.com:61088/adressa-articles")
@@ -37,11 +40,48 @@ print("Number of users:", user_profiles.count())
 
 countInCollection = defaultdict(int)
 idf = {}
-for article in articles.find():
-    for profile in article["profiles"]:
-        countInCollection[(profile["item"])] += 1
+counter = 0
+keywords = []
+article_map = {}
 
-N = articles.count()
-for key in countInCollection:
-    idf[key] = math.log2(N / countInCollection[key])
+for article in articles.find():
+    if article["keywords"] is not None:
+        article_map[counter] = article["_id"]
+        keywords.append(article["keywords"])
+        counter += 1
+
+dictionary = Dictionary(keywords)
+corpus = list(map(lambda doc: dictionary.doc2bow(doc), keywords))
+tfidf_model = TfidfModel(corpus)
+tfidf_corpus = list(map(lambda c: tfidf_model[c], corpus))
+tfidf_similarity = MatrixSimilarity(tfidf_corpus)
+
+query_corpus = dictionary.doc2bow(['årets trønder', 'bybrann'])
+query_tfidf = tfidf_model[query_corpus]
+print(query_tfidf)
+
+similarity = enumerate(tfidf_similarity[query_tfidf])
+query_result = sorted(similarity, key=lambda kv: -kv[1])[:5]
+
+results = list(map(lambda result: article_map[result[0]], query_result))
+for result in results:
+    pprint(articles.find_one({"_id": result})["title"])
+
+#pprint(tdif_similarity)
+#print(corpus[6])
+
+#pprint(keywords)
+
+
+#    for profile in article["profiles"]:
+#        countInCollection[(profile["item"])] += 1
+#        break
+#    break
+
+#N = articles.count()
+#for key in countInCollection:
+#    idf[key] = math.log2(N / countInCollection[key])
+
+
+
 
