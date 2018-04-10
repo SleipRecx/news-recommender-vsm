e0e8cc8582f511117570c6659ebd6126d050d2c1
Espen Meidell <espen.meidell@gmail.com>
2018-04-10 18:27:03 +0200

    Add query to get n most popular articles

--- src/MostPopular.py
+++ src/MostPopular.py
@@ -2,10 +2,10 @@
 # Created: 10.04.18 17:42
 # Owner: Espen Meidell
 from collections import defaultdict
-from mongo_queries import db
+from mongo_queries import db, get_n_most_popular
 
-if __name__ == '__main__':
 
+def create_most_popular():
     c_user = db.user_profiles.count()
     count = defaultdict(int)
     c = 0
@@ -16,7 +16,11 @@ if __name__ == '__main__':
         c += 1
     article_ids = list(count.keys())
     article_ids.sort(key=lambda x: count[x])
-    most_pop = article_ids[len(article_ids)-1000:]
+    most_pop = article_ids[len(article_ids) - 1000:]
     most_pop.reverse()
     for i in range(len(most_pop)):
         db.most_popular.insert({"_id": i, "articleId": most_pop[i]})
+
+
+if __name__ == '__main__':
+    get_n_most_popular(10)
