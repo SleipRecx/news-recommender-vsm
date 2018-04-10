e1b1e937322629707f7dabfa56a4935ef570ec0f
Espen Meidell <espen.meidell@gmail.com>
2018-04-10 18:16:51 +0200

    Add code to find most popular articles

---
+++ src/MostPopular.py
@@ -0,0 +1,22 @@
+# Project: web-intelligens
+# Created: 10.04.18 17:42
+# Owner: Espen Meidell
+from collections import defaultdict
+from mongo_queries import db
+
+if __name__ == '__main__':
+
+    c_user = db.user_profiles.count()
+    count = defaultdict(int)
+    c = 0
+    for user in db.user_profiles.find():
+        print(c, "/", c_user)
+        for event in user["events"]:
+            count[event["articleId"]] += 1
+        c += 1
+    article_ids = list(count.keys())
+    article_ids.sort(key=lambda x: count[x])
+    most_pop = article_ids[len(article_ids)-1000:]
+    most_pop.reverse()
+    for i in range(len(most_pop)):
+        db.most_popular.insert({"_id": i, "articleId": most_pop[i]})
